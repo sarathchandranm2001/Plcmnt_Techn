25a04f373a7f32188f3e25022bd70f14e34213d6
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-05-31 21:45:13 +0530

    updated

---
+++ c/basics/primenumber.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int checkPrime(int num)
+{
+    int flag=1;
+    if (num <= 1)
+    return 0; 
+    for(int i=2;i<num/2;i++){
+        if(num%i==0){
+            flag=0;
+            break;
+        }
+    }
+    return flag;
+}
+
+
+void main()
+{   
+    int num;
+    int result;
+    scanf("%d",&num);
+    result=checkPrime(num);
+    printf("%d",result);
+}
+
